9ccd5f43faf05de4e792c5c35dec7b05eff7d5e9
Daviloo <you@example.com>
2026-05-10 03:01:10 +0200

    ++

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,23 @@
 (() => {
+  // Año en el pie
   const year = document.getElementById('year');
   if (year) {
     year.textContent = String(new Date().getFullYear());
   }
 
+  // Validación del formulario
   const forms = document.querySelectorAll('.needs-validation');
   Array.from(forms).forEach((form) => {
     form.addEventListener(
       'submit',
       (event) => {
+        // Si hay errores, no envía
         if (!form.checkValidity()) {
           event.preventDefault();
           event.stopPropagation();
         }
+
+        // Muestra los avisos
         form.classList.add('was-validated');
       },
       false,
